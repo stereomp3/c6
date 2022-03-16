05538892bc46b0918d4953c79eae16fccec401e3
stereomp3 <ericjjkkhandsome@gmail.com>
2022-03-16 21:39:34 +0800

    add do while

--- c6.c
+++ c6.c
@@ -340,11 +340,11 @@ void stmt() {
     a = e + 1;
     stmt();
     //*++e = BZ; b = ++e;
-    if (tk == While) next(); else { printf("%d: open paren expected\n", line); exit(-1); }
+    if (tk == While) next(); else { printf("%d: open while expected\n", line); exit(-1); }
     if (tk == '(') next(); else { printf("%d: open paren expected\n", line); exit(-1); }
     expr(Assign);
     if (tk == ')') next(); else { printf("%d: close paren expected\n", line); exit(-1); }
-    if (tk == ';') next(); else { printf("%d: close paren expected\n", line); exit(-1); }
+    if (tk == ';') next(); else { printf("%d: close ; expected\n", line); exit(-1); }
     //*++e = JMP; *++e = (int)a;
     *++e = BNZ; *++e = (int)a; // BNZ是代表不是 0 的時候跳，這裡是跳入迴圈
   }
--- test/dowhile.c
+++ test/dowhile.c
@@ -2,14 +2,12 @@
 
 int main()
 {
-  int s;
   int i;
   i = 0;
-  s = 1;
   do {
-    s = i + s;
     i = i + 1;
-    printf("do while: s=%d i=%d\n",s,i);
-  } while(i < 10);
+    printf("i=%d\t", i);
+  } while (i < 10);
+  
   return 0;
 }
