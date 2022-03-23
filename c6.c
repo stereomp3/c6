b778e843a4d5b161ca4e8a56cfdd6f3783afa886
stereomp3 <ericjjkkhandsome@gmail.com>
2022-03-23 16:21:09 +0800

    u

--- c6.c
+++ c6.c
@@ -335,6 +335,17 @@ void stmt() {
     *++e = JMP; *++e = (int)a;
     *b = (int)(e + 1);
   }
+  else if (tk == Do){  
+    next();
+    a = e + 1;
+    stmt();
+    if (tk == While) next(); else { printf("%d: open while expected\n", line); exit(-1); }
+    if (tk == '(') next(); else { printf("%d: open paren expected\n", line); exit(-1); }
+    expr(Assign);
+    if (tk == ')') next(); else { printf("%d: close paren expected\n", line); exit(-1); }
+    if (tk == ';') next(); else { printf("%d: close ; expected\n", line); exit(-1); }
+    *++e = BNZ; *++e = (int)a; // BNZ是代表不是 0 的時候跳，這裡是跳入迴圈
+  }
   else if (tk == Return) { // return 語句
     next();
     if (tk != ';') expr(Assign);
